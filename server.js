56681dbb1a8e32292772d4a81cfe1cc8c1270a8a
Mykyta Varnikov <varnikov@sheridancollege.ca>
2026-04-06 18:39:33 -0400

    test2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.send('Hello, Dockerized Node.js APP! Actions Workflow is completed. Ыпыкшощывшопышропышопыгрыпыпгр');
+    res.send('Hello, Dockerized Node.js APP! Actions Workflow is completed. April 06 Test');
 });
 
 app.listen(PORT, () => {
