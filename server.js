7b51122837180431870cbc0481be482a0eeec4b8
Mykyta Varnikov <varnikov@sheridancollege.ca>
2026-04-06 18:26:08 -0400

    update server logic

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.send('Hello, Dockerized Node.js APP! Actions Workflow is completed.');
+    res.send('Hello, Dockerized Node.js APP! Actions Workflow is completed. TEST');
 });
 
 app.listen(PORT, () => {
